4fd9b44fa72cf93327c947701f7c850aa2fad5fd
abhijeetg2005 <46431172+abhijeetg2005@users.noreply.github.com>
2019-01-07 12:47:38 +1100

    Initial

---
+++ BAMActivityExtractor.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace BAMActivityExtractApp
+{
+    partial class BAMActivityExtractor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtConnString = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BAMPrimaryImport DB Connection String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Path to extract BAM Activity Definition";
+            // 
+            // txtConnString
+            // 
+            this.txtConnString.Location = new System.Drawing.Point(218, 34);
+            this.txtConnString.Name = "txtConnString";
+            this.txtConnString.Size = new System.Drawing.Size(266, 20);
+            this.txtConnString.TabIndex = 2;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(218, 96);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(193, 20);
+            this.txtPath.TabIndex = 3;
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(408, 137);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(417, 94);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(66, 23);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 175);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtConnString);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "BAM Activity Extractor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtConnString;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
+
---
+++ BAMActivityExtractor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAMActivityExtractApp
+{
+    public partial class BAMActivityExtractor : Form
+    {
+        string ActivityName = "";
+        string ActivityDefnXML = "";
+        string ViewName = "";
+        string ViewDefnXML = "";
+        string path = "";
+        public BAMActivityExtractor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            int counter = 0;
+            try
+            {
+                if (txtConnString.Text != null)
+                {
+                    connection.ConnectionString = txtConnString.Text;// @"Data Source=[SQLSERVERNAME]\[INSTANCENAME];Initial Catalog=BAMPrimaryImport;Integrated Security=SSPI;Persist Security Info=False;Password=[PASSWORD];User ID=[USERNAME];";
+                }
+                
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "select A.ActivityName,A.DefinitionXml,V.ViewName,V.DefinitionXml from bam_Metadata_Activities A inner join bam_Metadata_ActivityViews AV on AV.ActivityName = A.ActivityName INNER JOIN bam_Metadata_Views V ON AV.ViewName = V.ViewName ";
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        ActivityName = reader.GetString(0);
+                        ActivityDefnXML = reader.GetString(1);
+                        ViewName = reader.GetString(2);
+                        ViewDefnXML = reader.GetString(3);
+
+                        path = txtPath.Text + "\\bam_" + ActivityName + ".xml";  
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            sw.WriteLine("<BAMDefinition xmlns = 'http://schemas.microsoft.com/BizTalkServer/2004/10/BAM' > ");
+                            sw.WriteLine(ActivityDefnXML);
+                            sw.WriteLine(ViewDefnXML);
+                            sw.WriteLine("</BAMDefinition>");
+                        }
+                        counter++;
+                    }
+                    
+                    MessageBox.Show(counter.ToString() + " Activity Definitions extracted!");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.EventLog.WriteEntry("SQL", ex.InnerException.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
